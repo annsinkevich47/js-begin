0aa2c7e572fe200215250b19085f65dcbe826db6
annsinkevich47 <anneta.sinkevich@mail.ru>
2023-11-06 22:18:55 +0300

    movie DataBase

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 "use strict";
 
-const category = "toys";
-console.log(`https://someurl.com/${category}/5`);
+let numberOfFilms = prompt ("Сколько фильмов вы уже посмотрели?", " ");
 
-const user = "Ivan";
-alert(`${user}`);
+let lastMovieWatched = prompt ("Один из последних просмотренных фильмов?", " ");
+let lastMovieWatched2 = prompt ("Один из последних просмотренных фильмов?", " ");
+
+let movieRate = prompt ("На сколько бы вы их оценили?", " ");
+let movieRate2 = prompt ("На сколько бы вы их оценили?", " ");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+    } ,
+    actors: {
+
+    },
+    genres: [],
+	privat: false
+}
+
+personalMovieDB.movies[lastMovieWatched] = movieRate;
+personalMovieDB.movies[lastMovieWatched2] = movieRate2;
+
+
+console.log (personalMovieDB);
